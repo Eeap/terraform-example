a00831018531c42a37c99200d599ee796fff1c93
Eeap <kdg97811@naver.com>
2023-04-12 23:37:20 +0900

    refactor: add security group

--- main.tf
+++ main.tf
@@ -5,8 +5,26 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami = "ami-830c94e3"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.example_sg.id]
 
+  user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World!" > index.html
+                nohup busybox httpd -f -p 8080 &
+                EOF
   tags = {
     Name = "terraform-example"
   }
+}
+
+resource "aws_security_group" "example_sg" {
+    name = "terraform-example-instance"
+
+    ingress {
+      cidr_blocks = [ "0.0.0.0/0" ]
+      from_port = 8080
+      protocol = "tcp"
+      to_port = 8080
+    } 
+  
 }
